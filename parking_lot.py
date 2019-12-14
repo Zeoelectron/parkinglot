3bf22e7798be22cd7e438b8848b99e29c003df0a
Zeoelectron <58879278+Zeoelectron@users.noreply.github.com>
2019-12-14 20:06:29 +0530

    Added text file as input functionality

--- parking_lot.py
+++ parking_lot.py
@@ -1,3 +1,20 @@
+import sys # for giving text file as input and output
+
+outputfile = None # Initializing output file
+try:
+    outputfile = open(sys.argv[2], "a") # creates output file if output argument is given
+except:
+    pass
+def prnt(str): # custom print function to print either into the command line or a txt file
+    try:
+        sys.argv[1] # checks if argument input exists and prints us the response in the commandline if it doesn't
+        global outputfile
+        if outputfile == None:
+            outputfile = open(sys.argv[2], "a")
+        outputfile.write(str+"\n")
+    except:
+        print(str)
+
 class car:
     def __init__(self, reg_number, color):
         self.reg_number = reg_number
@@ -17,7 +34,7 @@ class parkinglot:
         self.number_of_slots = number_of_slots
         self.dict_of_parked_cars = {}
         self.num_of_cars_in_parking = 0
-        print("Created a parking lot with ",number_of_slots," slots")
+        prnt("Created a parking lot with "+str(number_of_slots)+" slots")
         # for i in range(number_of_slots):
         #     self.list_of_parked_cars.append(None)
 
@@ -27,10 +44,10 @@ class parkinglot:
             nearest_freeslot = self.find_nearest_freeslot()
             # self.list_of_parked_cars[self.num_of_cars_in_parking] = self.issueticket(car_being_parked, nearest_freeslot)
             self.dict_of_parked_cars[nearest_freeslot] = car_being_parked
-            print("Allocated slot number: ",nearest_freeslot)
+            prnt("Allocated slot number: "+str(nearest_freeslot))
             self.num_of_cars_in_parking+=1
         else:
-            print("Sorry, parking lot is full")
+            prnt("Sorry, parking lot is full")
 
     '''
     slots_occupied = {}
@@ -70,46 +87,46 @@ class parkinglot:
     def leave(self, slotnumber):
         try:
             del self.dict_of_parked_cars[slotnumber]
-            print("Slot number ",slotnumber," is free")
+            prnt("Slot number "+str(slotnumber)+" is free")
             self.num_of_cars_in_parking -= 1
         except:
-            print("Not Found")
+            prnt("Not Found")
 
     def status(self):
-        print("Slot  "+"Registration  "+"Color")
+        prnt("Slot  "+"Registration  "+"Color")
         for x in self.dict_of_parked_cars:
             car = self.dict_of_parked_cars[x]
-            print(x,"     ", car.reg_number, "      ", car.color) # have to change this
+            prnt(str(x) +"     " + car.reg_number+ "      " + car.color) # have to change this
 
     def registration_numbers_for_cars_with_colour(self, color):
         found = False
         for x in self.dict_of_parked_cars:
             car = self.dict_of_parked_cars[x]
             if car.color == color :
-                print(car.reg_number)
+                prnt(car.reg_number)
                 found = True
         if (found == False):
-            print("Not Found")
+            prnt("Not Found")
 
     def slot_numbers_for_cars_with_colour(self, color):
         found = False
         for x in self.dict_of_parked_cars:
             car = self.dict_of_parked_cars[x]
             if car.color == color :
-                print(x)
+                prnt(x)
                 found = True
         if(found == False):
-            print("Not Found")
+            prnt("Not Found")
 
     def slot_number_for_registration_number(self, reg_number):
         found = False
         for x in self.dict_of_parked_cars:
             car = self.dict_of_parked_cars[x]
             if car.reg_number == reg_number :
-                print(car.reg_number)
+                prnt(car.reg_number)
                 found = True
         if(found == False):
-            print("Not Found")
+            prnt("Not Found")
 
 default_parking_lot = None
 
@@ -117,8 +134,7 @@ def create_parking_lot(number_of_slots):
     global default_parking_lot
     default_parking_lot = parkinglot(number_of_slots)
 
-while True:
-    inp = input()
+def commandline(inp):
     split_input = inp.split()
     if inp.startswith("create_parking_lot "):
         create_parking_lot(split_input[1]) # regular expression to extract number from the command given
@@ -134,6 +150,17 @@ while True:
         default_parking_lot.slot_numbers_for_cars_with_colour(split_input[1])
     elif inp.startswith("slot_number_for_registration_number "):
         default_parking_lot.slot_number_for_registration_number(split_input[1])
+
+try:
+    sys.argv[1] # checks if the input argument exists
+    with open(sys.argv[1], "r") as inputfile:
+        inplist = inputfile.read().split("\n")
+    for i in range(len(inplist)):
+        commandline(inplist[i])
+except:
+    while True:
+        inp = input()
+        commandline(inp)
 
 
 
